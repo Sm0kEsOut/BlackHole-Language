dd0c25676a71e9068ee1239091d354e49471f211
Brody Gaston <gaston.brody@gmail.com>
2025-03-05 18:45:18 -0800

    Fixed syntax

--- test/compiler.test.js
+++ test/compiler.test.js
@@ -3,10 +3,10 @@ const { expect } = require('chai');
 
 describe('Lexer', () => {
   it('should tokenize a simple expression', () => {
-    const input = 'var x = 42;';
+    const input = 'num x = 42;';
     const tokens = lexer(input);
     expect(tokens).to.deep.equal([
-      { type: 'Keyword', value: 'var', line: 1, column: 1 },
+      { type: 'Keyword', value: 'num', line: 1, column: 1 },
       { type: 'Identifier', value: 'x', line: 1, column: 5 },
       { type: 'Operator', value: '=', line: 1, column: 7 },
       { type: 'Number', value: '42', line: 1, column: 9 },
