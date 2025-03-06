f4ef76a5e7d391efd9b447cce5e27433787e0f8d
Brody Gaston <gaston.brody@gmail.com>
2025-03-06 01:16:33 -0800

    Update compiler.test.js

--- test/compiler.test.js
+++ test/compiler.test.js
@@ -3,15 +3,15 @@ const { expect } = require('chai');
 
 describe('Lexer', () => {
   it('should tokenize a simple expression', () => {
-    const input = 'local x = "we";';
+    const input = 'int x = 45;';
     const tokens = lexer(input);
     expect(tokens).to.deep.equal([
-      { type: 'Keyword', value: 'local', line: 1, column: 1 },
-      { type: 'Identifier', value: 'x', line: 1, column: 7 },
-      { type: 'Operator', value: '=', line: 1, column: 9 },
-      { type: 'Number', value: 'we', line: 1, column: 11 },
-      { type: 'Punctuation', value: ';', line: 1, column: 13 },
-      { type: 'EOF', value: null, line: 1, column: 14 }
+      { type: 'Keyword', value: 'int', line: 1, column: 1 },
+      { type: 'Identifier', value: 'x', line: 1, column: 5 },
+      { type: 'Operator', value: '=', line: 1, column: 7 },
+      { type: 'Number', value: '45', line: 1, column: 9 },
+      { type: 'Punctuation', value: ';', line: 1, column: 11 },
+      { type: 'EOF', value: null, line: 1, column: 12 }
     ]);
   });
 });
